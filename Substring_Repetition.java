df6a5518f275a56b24dec2ff268ba695cff23cb6
arkdevelopment <arkdevelop@users.noreply.github.com>
2018-09-30 11:53:08 -0400

    Update Substring_Repetition.java

--- Substring_Repetition.java
+++ Substring_Repetition.java
@@ -2,14 +2,17 @@ import java.lang.*;
 import java.io.*;
 import java.util.*;
 
-/*Question: Given a string A consisting of n characters and a string B consisting of m characters, write a function that will 
+/*
+Question: Given a string A consisting of n characters and a string B consisting of m characters, write a function that will 
 return the number of times A must be stated such that B is a substring of the repeated A. 
 If B can never be a substring, return -1.
 Example:
 A = ‘abcd’
 B = ‘cdabcdab’
 The function should return 3 because after stating A 3 times, getting ‘abcdabcdabcd’, B is now a substring of A.
-You can assume that n and m are integers in the range [1, 1000]. */
+You can assume that n and m are integers in the range [1, 1000]. 
+Question link: https://www.geeksforgeeks.org/google-interview-experience-set-7-software-engineering-intern/
+*/
 
 class Solution {
     public int solution(String A, String B) {
